3615d6f65794fd82bc5a4898c8e9d151c76b8152
Yao Z.C <yaozhichao418@163.com>
2021-03-29 10:10:45 +0800

    日志装饰器

--- IO/log/main.py
+++ IO/log/main.py
@@ -2,7 +2,7 @@
 """
     Author: Yao Z.C.
     Date: 2021-03-26 11:19:09
-    LastEditTime: 2021-03-26 17:11:38
+    LastEditTime: 2021-03-29 10:10:27
     FilePath: /pyPackage/IO/log/main.py
 """
 import sys, os
@@ -11,6 +11,20 @@ import logging.config
 
 logging.config.fileConfig("config/logging_config.ini")
 logger = logging.getLogger("developer")
+
+from datetime import datetime
+def write2log(func):
+    def int_time(*args, **kwargs):
+        time0 = datetime.now()
+        logger.info("{} 开始运行······".format(func))
+        res = None
+        try:
+            res = func(*args, **kwargs)
+        except Exception as error:
+            logger.exception(sys.exc_info())
+        logger.info("{} 运行耗时：{}".format(func, datetime.now()-time0))
+        return res
+    return int_time
 
 
 logger.debug('This is debug message')
